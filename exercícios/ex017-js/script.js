b3ba336e28ca046cb8f45d41eaa2fa74862928fa
João Paulo Cordeiro <joaopaulocordeiro_webdev@hotmail.com>
2024-10-30 22:42:21 -0300

    aumentei a tabuada para ate o 100x

--- exercícios/ex017-js/script.js
+++ exercícios/ex017-js/script.js
@@ -1,6 +1,7 @@
 function tabuada() {
     let num = document.getElementById('inum')
     let tab = document.getElementById('seltab')
+    let tab2 = document.getElementById('seltab2')
     let res = document.getElementById('seltab')
 
     if (num.value.length == 0) {
@@ -8,11 +9,20 @@ function tabuada() {
     } else {
         let n = Number(num.value)
         tab.innerHTML = ' '
-        for (c = 1; c <= 20; c++) {
+        for (c = 1; c <= 50; c++) {
             let item = document.createElement('option')
             item.text = `${n} X ${c} = ${n * c}`
             item.value = `tab${c}`
             tab.appendChild(item)
         }
+
+        tab2.innerHTML = ' '
+        for (c = 51; c <= 100; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n * c}`
+            item.value = `tab1${c}`
+            tab2.appendChild(item)
+        }
+    
     }
 }
